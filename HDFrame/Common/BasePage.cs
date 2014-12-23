3c5f96d0576ee5af2b996b2157a9b6664d7edf5e
吴炅明 <wu922008@126.com>
2014-12-23 17:42:10 +0800

    add new code Signed-off-by: 吴炅明 <wu922008@126.com>

---
+++ HDFrame/Common/BasePage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HDFrame.Common
+{
+    public class BasePage : Page
+    {
+        #region ASP.NET 事件
+
+        /// <summary>
+        /// 初始化方法（先于Page_Load和OnLoad执行）
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            this.EnableViewState = false;   // 禁用ViewState
+        }
+
+        /// <summary>
+        /// 异常处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void Page_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+
+            /*--写事件日志开始--*/
+
+            throw ex;
+        }
+
+        /// <summary>
+        /// 渲染前触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void Page_PreRender(object sender, EventArgs e)
+        {
+        }
+
+        //去缓存
+        protected override void OnLoad(EventArgs e)
+        {
+            Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+            Response.Cache.SetNoStore();
+            Response.Expires = 0;
+            base.OnLoad(e);
+        }
+
+        #endregion 事件
+    }
+}
---
+++ HDFrame/Common/GridJsonStruct.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace HDFrame.Common
+{
+    /// <summary>
+    /// Grid需要的Json数据结构
+    /// </summary>
+    public class GridJsonStruct
+    {
+        public readonly int total;
+        public readonly object items;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dt">DataTable</param>
+        /// <param name="_total">数据条数</param>
+        public GridJsonStruct(DataTable dt, int _total)
+        {
+            if (dt != null)
+                this.items = dt;
+            total = _total;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj">数据集对象</param>
+        /// <param name="_total">数据条数</param>
+        public GridJsonStruct(object obj, int _total)
+        {
+            if (obj != null)
+                this.items = obj;
+            total = _total;
+        }
+
+        /// <summary>
+        /// 获取条数
+        /// </summary>
+        public int Total
+        {
+            get
+            {
+                return total;
+            }
+        }
+        /// <summary>
+        /// 获取数据集
+        /// </summary>
+        public object Items
+        {
+            get
+            {
+                return items;
+            }
+        }
+        /// <summary>
+        /// 用于扩展
+        /// </summary>
+        public string extend { get; set; }
+    }
+}
